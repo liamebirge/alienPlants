a4a6fb94700e1d7dbf843243d36d681baa9c37d7
Liam English-Birge <liamebirge@gmail.com>
2017-11-24 19:16:26 -0800

    starting about page

--- JS/script.js
+++ JS/script.js
@@ -1,9 +1,15 @@
+function chooseImg(){
+	var randNum = Math.floor((Math.random() * 10) + 1);
+	document.getElementById("changeImg").src = "data/plants/plant" + randNum + ".JPG";
+	document.getElementById("mpLink").href = "HTML/plantPages/plant" + randNum + ".html";
+}
+
 function gotoSame(url){
-  var win = window.open(url, '_self');
-  win.focus();
+	var win = window.open(url, '_self');
+    win.focus();
 }
 
 function gotoNew(url){
-  var win = window.open(url, '_blank');
-  win.focus();
+    var win = window.open(url, '_blank');
+    win.focus();
 }
