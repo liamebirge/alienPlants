a97bd02efb4634dbb478a94efda59d71a3de71b1
Liam English-Birge <liamebirge@gmail.com>
2017-11-25 13:54:15 -0800

    tidying up icons

--- JS/script.js
+++ JS/script.js
@@ -13,11 +13,13 @@ function gotoNew(url){
     var win = window.open(url, '_blank');
     win.focus();
 }
+//random icon
 (function() {
 	var randNum = Math.floor((Math.random() * 16) + 1);
     var link = document.querySelector("link[rel*='icon']") || document.createElement('link');
     link.type = 'image/png';
     link.rel = 'shortcut icon';
-    link.href = '../data/icons/icon' + randNum + ".png";
+    link.href = 'https://raw.githubusercontent.com/liamebirge/alienPlants/master/data/icons/icon' + randNum + ".png";
+    //https://raw.githubusercontent.com/liamebirge/alienPlants/master/data/icons/icon1.png
     document.getElementsByTagName('head')[0].appendChild(link);
 })();
