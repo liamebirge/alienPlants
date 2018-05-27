93f367b928b808f1d74188d8a7c22adf6c57cdf6
LiamEBirge <liamebirge@gmail.com>
2018-05-27 15:08:05 -0700

    Context menu

--- JS/script.js
+++ JS/script.js
@@ -43,3 +43,14 @@ function calLink(){
     var cYear = date.getFullYear();
     gotoNew("Calendar/" + cMonth + "-" + cYear + ".html");
 }
+if (document.addEventListener) { // IE >= 9; other browsers
+    document.addEventListener('contextmenu', function(e) {
+    alert("You've tried to open context menu"); //here you draw your own menu
+    e.preventDefault();
+}, false);
+} else { // IE < 9
+    document.attachEvent('oncontextmenu', function() {
+    alert("You've tried to open context menu");
+    window.event.returnValue = false;
+    });
+}
